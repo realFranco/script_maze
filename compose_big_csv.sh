13020c6d605a1193f0adee7b439e9db5790c11f2
franco <f97gp1@gmail.com>
2021-08-04 09:19:17 -0400

    Improve, shell script expandido

--- compose_big_csv.sh
+++ compose_big_csv.sh
@@ -1,46 +1,67 @@
 echo "This will be the script container of the solutions"
 echo ""
 
+# Example of trim and echos
+# IN="bla@some.com;john@home.com"
+# mails=$(echo $IN | tr ";" "\n")
+
 # Fetch executables c++ scripts
 # cpp_set=$(find -iname "*.cpp")
 
 # Fetch executables perl scripts
 perl_set=$(find -iname "*.pl")
-perl_set=$(echo $perl_set | tr "\n" "\n") # trim
+# perl_set=$(echo $perl_set | tr "\n" "\n") # trim
 
 # Fetch executables python scripts
 python_set=$(find . -iname "*[^__].py")
-python_set=$(echo $python_set | tr "\n" "\n") # trim
+# python_set=$(echo $python_set | tr "\n" "\n") # trim
 
+# Compose the whole set of scripts to be running
+script_container="$perl_set \n $python_set"
+script_container=$(echo $script_container | tr "\n" "\n") # trim
 
-for script in $python_set
+# echo "$script_container"
+
+for script in $script_container
 do
-    if [[ "$script" == *".py" ]]; then
-        echo "We are running a python code"
-        python3 $script >> main/output/csv.csv
-    fi
-    
+    case $script in
+
+        *".cpp")
+            echo -n "We are running a C++ code"
+            # g++ $x -o object
+            # ./object >> main/output/csv.csv
+            echo ""
+            ;;
+
+        *".java")
+            echo -n "We are running a Java code"
+            echo ""
+            ;;
+
+        *".pl")
+            echo -n "We are running a Perl code"
+            echo ""
+            ;;
+
+        *".py")
+            echo -n "We are running a Python code"
+            # python3 $script >> main/output/csv.csv
+            echo ""
+            ;;
+
+        *".r")
+            echo -n "We are running a R code"
+            echo ""
+            ;;
+
+        *".rb")
+            echo -n "We are running a Ruby code"
+            echo ""
+            ;;
+
+        *)
+            echo -n "unknown $script"
+            echo ""
+            ;;
+    esac
 done
-
-
-# Fetch executables c++ scripts
-# cpp_set=$(find -iname "*.cpp")
-
-
-# gcc main/some_content_3.c -o main/out
-
-# Execute perl
-# perl function_1.pl
-
-# for x lopp [ "hello_world.cpp", hello_world_2.cpp, hello_world_3.cpp ]:
-
-#     g++ $x -o object
-
-#     var = 'asdasd'
-#     ./obejct > "$var" // archivo intermedio
-
-# ./main/out
-
-# Example of trim and echos
-# IN="bla@some.com;john@home.com"
-# mails=$(echo $IN | tr ";" "\n")
